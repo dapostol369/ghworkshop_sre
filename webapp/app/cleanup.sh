8ef8b4e7e8d62cc3e16d53fdc38c272e9f61899e
maychu <maychu@us.ibm.com>
2017-10-02 21:09:06 -0700

    cleanup file

--- webapp/app/cleanup.sh
+++ webapp/app/cleanup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 . ./variables.sh
-. ./remove_images.sh
 . ./remove_containers.sh
 echo "Time: $(date)" >>$LOG_FILE
 echo "**************** INITIATING CLEANUP ***********************"
 
 remove_containers
-# remove_images
-
-# rm $LOG_FILE
 
 echo "**************** CLEANUP COMPLETED ***********************"
